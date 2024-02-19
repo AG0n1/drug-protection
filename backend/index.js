1ac4218c35996d1df68fb2b3c5a7a90cb80d4a90
mathew markovets <mmarkovec15072003@gmail.com>
2024-02-19 14:40:58 +0300

    tg
    commands list added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/api', (req, res) => {
+  res.json(users);
+});
+
 const users = {
   AG0n1: {
     name: "AG0n1",
@@ -29,14 +33,6 @@ const users = {
   },
 };
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: 'mmarkovec15072003@gmail.com',
-    pass: '8Puvjgppy'
-  }
-})
-
 app.post('/api', (req, res) => {
   const { email, password } = req.body;
   const user = Object.values(users).find((u) => u.email === email && u.password === password);
@@ -49,9 +45,56 @@ app.post('/api', (req, res) => {
   }
 });
 
-app.get('/api', (req, res) => {
-  res.json(users);
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: 'mmarkovec15072003@gmail.com',
+//     pass: '8Puvjgppy'
+//   }
+// })
+
+app.get('/employee', (req, res) => {
+  res.json(employee);
 });
+
+const employee = {
+  8472: {
+    name: "Helen",
+    second_name: "Junior",
+    status: "psycologist",
+    phone_number: "+375283774837",
+    email: "helenjunior@gmail.com"
+  },
+
+  3832: {
+    name: "Matthew",
+    second_name: "Markovets",
+    status: "admin",
+    phone_number: "+3752923883088",
+    email: "matveymarkovets@gmail.com"
+  },
+
+  4324: {
+    name: "Kirill",
+    second_name: "Mushits",
+    status: "customer servise",
+    phone_number: "+375283774837",
+    email: "kirillmushits@gmail.com"
+  }
+}
+
+app.post('/employee', (req,res) => {
+  const id = req.body;
+  const isEmployee = Object.values(employee).find((u) => u === id)
+  if (isEmployee) {
+    res.json({
+      name: employee.id.name,
+      second_name: employee.id.second_name
+    })
+  } else {
+    res.json({employee: null})
+  }
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- telegram/bot.js
+++ telegram/bot.js
@@ -10,7 +10,65 @@ class User {
     this.userStatus = '';
   }
 
-  askForEmail(botInstance) {
+  sayHello(msg, chatId) {
+    bot.sendMessage(chatId, `Здравствуйте, ${msg.from.first_name}`)
+  }
+  
+}
+
+const user = new User();
+
+bot.onText(/\/start/, (msg) => {
+  const { chatId } = msg.chat;
+
+  bot.sendMessage(`Зравствуйте, ${msg.from.first_name}, это помощник DrugFree! Этот бот был разработан для помощи людям с разной формой  `)
+
+})
+
+bot.onText(/\/register/, (msg) => {
+  const { chatId } = msg.chat;
+
+  bot.sendMessage(`Зравствуйте, ${msg.from.first_name}, это помощник DrugFree! Этот бот был разработан для помощи людям с разной формой  `)
+
+})
+
+bot.onText(/\/donate/, (msg) => {
+  const { chatId } = msg.chat;
+
+  bot.sendMessage(`Зравствуйте, ${msg.from.first_name}, это помощник DrugFree! Этот бот был разработан для помощи людям с разной формой  `)
+
+})
+
+bot.onText(/\/search/, (msg) => {
+  const { chatId } = msg.chat;
+
+  bot.sendMessage(`Зравствуйте, ${msg.from.first_name}, это помощник DrugFree! Этот бот был разработан для помощи людям с разной формой  `)
+
+})
+
+bot.onText(/\/help/, (msg) => {
+  const { chatId } = msg.chat;
+
+  bot.sendMessage(`Зравствуйте, ${msg.from.first_name}, это помощник DrugFree! Этот бот был разработан для помощи людям с разной формой  `)
+
+})
+
+
+bot.on('message', (msg) => {
+  chatId = msg.chat.id
+  console.log(msg)
+  user.sayHello(msg, chatId)
+})
+
+
+
+function debug(obj = {}) {
+   return JSON.stringify(obj, null, 4)
+}
+
+/*
+
+askForEmail(botInstance) {
     return new Promise((res) => {
       botInstance.on('message', (msg) => {
         this.email = msg.text
@@ -34,16 +92,9 @@ class User {
       });
     });
   }
-}
-
-const user = new User();
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  if (chatId == 5194729836) {
-    bot.sendMessage(chatId, "Привет, Ричард!")
-    return
-  }
   if (msg.text === '/start') {
     bot.sendMessage(chatId, "Вы зарегистрированы на нашем сайте?");
 
@@ -62,19 +113,78 @@ bot.on('message', async (msg) => {
         })
       })
 
-      if (isClient == "да") {
+      if (isClient == "сотрудник") {
         bot.sendMessage("Отправьте Ваш уникальный код:")
 
+        const checkForEmployee = await new Promise((res) => {
+          bot.on("message", (msg) => {
+            id = msg.text
+            
+            fetch("/emploee", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+              },
+              body: id,
+            }) 
+            .then((response) => response.json)
+            .then((data) => {
+              if (data) {
+                // TODO think about how to check customer
+                bot.sendMessage(chatId, `Здравствуйте, ${data.name}!`)
+                
+                if (data.userStatus == "admin") {
+                  
+                }
+              } else {
+                bot.sendMessage(chatId, "Пожалуйста, повторите либо вернитесь назад")
+              }
+            })
+          })
+        })
       } else {
         // написать логику для пользователей
       }
 
     } else if (isRegister === "нет") {
+      bot.sendMessage(chatId, "Ничего, давайте пройдём регистрацию тут. Скажите Ваше имя:")
+      const askForName = await new Promise(res => {
+        bot.on('message', msg => {
+
+        })
+      })
+
       await user.askForName(chatId, bot);
       console.log("Отправьте свой email: ");
       await user.askForEmail(bot)
     } else {
+
       bot.sendMessage(chatId, "Пожалуйста, отправьте верный ответ (Да или Нет):");
     }
   }
 });
+*/
+/*
+Команды:
+  1) /start
+    Возврат к началу
+
+  2) /register
+    Пройти регистрацию
+
+  3) /donate
+    Поддержать проект
+
+  4) /search
+    Найти сотрудника
+
+  5) /help
+    Вывести список команд
+
+    start - Возврат к началу
+    register - Пройти регистрацию
+    donate - Поддержать проект
+    search - Найти сотрудника
+    help - Вывести список команд
+*/
