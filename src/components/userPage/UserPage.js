4ce6010598f8a636400d7def4a36fecca3e920fd
mathew markovets <mmarkovec15072003@gmail.com>
2024-04-27 20:48:11 +0300

    Added a little styles in user page

--- src/components/userPage/UserPage.js
+++ src/components/userPage/UserPage.js
@@ -3,7 +3,19 @@ function UserPage() {
     return (
         <div className="userPage" >
             <div className="userInfo">
-                
+                <div className="userPhoto">
+
+                </div>
+
+                <div className="userData">
+                    <div className="userName">
+                        
+                    </div>
+
+                    <div className="userDescription">
+
+                    </div>
+                </div>
             </div>
         </div>
     )
