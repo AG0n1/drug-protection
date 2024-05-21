6c5fe1da3ad2c8df69d1fc97d7c3ea931492a203
mathew markovets <mmarkovec15072003@gmail.com>
2024-05-21 22:56:46 +0300

    Update Advertising elem

--- src/components/InfoTable/Advertising.js
+++ src/components/InfoTable/Advertising.js
@@ -2,6 +2,18 @@ import Image from "../Image"
 import logo from "../images/precent-removebg-preview.png"
 
 function Advertising() {
+    let isOpen = false
+    const open = () => {
+        let appointment = document.getElementById("appointment")
+        appointment.classList.remove("hidden")
+    }
+
+    const close = () => {
+        
+        let appointment = document.getElementById("appointment")
+        appointment.classList.add("hidden")
+    }
+
     return(
         <div className="advertising">
             <div className="advertising-title">
@@ -17,7 +29,7 @@ function Advertising() {
                     <div className="a-text-info">
                         Мы позаботимся о Ваших проблемах, а потому нам важно, чтобы Вы получили первую консультацию со скидкой
 
-                        <button>
+                        <button onClick={open}>
                             Записаться на приём
                         </button>
                     </div>
@@ -25,6 +37,12 @@ function Advertising() {
 
                 <div className="a-main-photo">
                     <img width="300px" src={logo} />
+                </div>
+            </div>
+
+            <div id="appointment" className="hidden formZone">
+                <div className="appointment-form">
+
                 </div>
             </div>
         </div>
