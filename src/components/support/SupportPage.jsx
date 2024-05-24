6a57de371045f9fd11fd39d37e7aea268b5904c5
mathew markovets <mmarkovec15072003@gmail.com>
2024-05-24 19:59:07 +0300

    Complited support page front

--- src/components/support/SupportPage.jsx
+++ src/components/support/SupportPage.jsx
@@ -1,8 +1,16 @@
 function SupportPage() {
+
+    const sendData = () => {
+        fetch('http://localhost:3001/suppornGetData', {
+            method: "POST",
+            body: JSON.stringify({})
+        })
+    }
+
     return (
         <div className="support-page">
             <form action="" className="support-form">
-                <div className="form-title">
+                <div className="form-title absoluteCenter">
                     Нам жаль, что вы столкнулись с неприятностью
 
                     <div className="form-title-small">
@@ -16,26 +24,40 @@ function SupportPage() {
                     </div>
 
                     <select>
-                        <option>
+                        <option value="1">
                             Проблема с сайтом
                         </option>
 
-                        <option>
+                        <option value="2">
                             Непрофессиональное поведение сотрудников
                         </option>
 
-                        <option>
+                        <option value="3">
                             Проблема с входом в личный кабинет
                         </option>
 
-                        <option>
+                        <option value="4">
                             Проблема с оплатой
                         </option>
 
-                        <option>
+                        <option value="5">
                             Другое
                         </option>
                     </select>
+                </div>
+
+                <div className="form-field">
+                    <div className="form-field-title">
+                        Пожалуйста, детально опишите Вашу проблему
+                    </div>
+
+                    <textarea className="support-userarea">
+
+                    </textarea>
+                </div>
+
+                <div className="form-field">
+                    <button onClick={sendData} id="form-send">Отправить</button>
                 </div>
             </form>
         </div>
