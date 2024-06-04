aa2862d1742c1ef3d63795ef827c5e93355b5b2d
mathew markovets <mmarkovec15072003@gmail.com>
2024-06-04 03:40:06 +0300

    Reafctor index.js

--- backend/index.js
+++ backend/index.js
@@ -3,152 +3,19 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-const fs = require("fs")
-const path = require("path");
-const e = require('cors');
-let activeUsers = {}
-const app = express();
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const messagesDir = path.join(__dirname, 'forum');
 const storiesFilePath = path.join(__dirname, 'stories', 'stories.txt');
 
-function generateToken() {
-  const symbols = '1234567890abcdefghijklmnopqrstuvwxyz';
-  let token = '';
-  for (let i = 0; i < 16; i++) {
-    token += symbols[Math.floor(Math.random() * symbols.length)];
-  }
-  return token;
-}
-
-app.post('/userSupportRequests', (req, res) => {
-  let { message, name, second_name, telegram_name } = req.body
-
-  connection.query("SELECT * FROM usersSupport", (err, result) => {
-    if (!err) {
-
-    } else {
-      console.log("Some error, ", err)
-    }
-  })
-
-})
-
-app.post('/getStoriesInfo', (req, res) => {
-  let { id } = req.body
-  fs.readFile(storiesFilePath + { id } + ".txt", 'utf8', (err, data) => {
-    if (err) {
-      console.error('Ошибка чтения файла:', err);
-      res.status(500).json({ data: 'Произошла ошибка при чтении данных.' });
-      return;
-    }
-
-    try {
-      const parsedData = JSON.parse(data);
-      res.json(parsedData);
-    } catch (parseError) {
-      console.error('Ошибка парсинга данных:', parseError);
-      res.status(500).json({ data: 'Произошла ошибка при парсинге данных.' });
-    }
-  });
-});
-
-app.post('/getMessages', (req, res) => {
-  let { id } = req.body
-
-  const filePath = path.join(messagesDir, `${id}.txt`);
-
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return res.status(500).json({ error: 'Error writing message to file' });
-    } else {
-      res.json(data)
-    }
-  });
-})
-
-app.post("/getSearchData", (req, res) => {
-  let { value } = req.body;
-  console.log(value);
-  const searchDir = 'backend/search/result.txt';
-  fs.readFile(searchDir, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Ошибка чтения файла' });
-    }
-
-    try {
-      const parsedData = JSON.parse(data);
-      let foundText = '';
-      let title = '';
-      for (let key in parsedData) {
-        if (key.toLowerCase().includes(value.toLowerCase())) {
-          console.log(key.toLowerCase(), value.toLowerCase());
-          foundText = parsedData[key];
-          title = key;
-          break;
-        }
-      }
-
-      if (!foundText) {
-        for (let key in parsedData) {
-          if (parsedData[key].content.toLowerCase().includes(value.toLowerCase())) {
-            foundText = parsedData[key].content;
-            title = key;
-            break;
-          }
-        }
-      }
-
-      if (foundText) {
-        res.status(200).json({ foundText, title });
-      } else {
-        res.status(200).json({ title: null })
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Ошибка при обработке данных' });
-    }
-  });
-});
-
-
 if (!fs.existsSync(messagesDir)) {
   fs.mkdirSync(messagesDir);
 }
-
-app.post('/saveMessage', (req, res) => {
-  const { message, date, time, name, second_name, id, status } = req.body;
-
-  if (message.trim().length === 0) {
-    return
-  }
-
-  const dataToWrite =
-    `"${time}": {
-  "name": "${name}", 
-  "second_name": "${second_name}", 
-  "date": "${date}", 
-  "message": "${message}",
-  "status": "${status}"
-},\n`;
-
-  const filePath = path.join(messagesDir, `${id}.txt`);
-  console.log(filePath)
-
-  fs.appendFile(filePath, dataToWrite, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return res.status(500).json({ error: 'Error writing message to file' });
-    }
-    console.log('Message saved to file:', dataToWrite);
-    res.json({ message: 'Message saved successfully' });
-  });
-});
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -166,165 +33,230 @@ connection.connect((err) => {
 });
 
 const secretKey = 'your_secret_key';
+let activeUsers = {};
 
-app.post('/signIn', (req, res) => {
-  const { email, password } = req.body;
+const generateToken = () => {
+  const symbols = '1234567890abcdefghijklmnopqrstuvwxyz';
+  return Array.from({ length: 16 }, () => symbols[Math.floor(Math.random() * symbols.length)]).join('');
+};
 
-  connection.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
-    if (error) {
-      console.error('Error in trying to connect to database:', error);
-      res.status(500).json({ error: 'Error in fetching' });
-    } else {
-      if (results.length > 0) {
-        const user = results[0];
+// Controllers
+const userSupportRequests = async (req, res) => {
+  const { message, name, second_name, telegram_name } = req.body;
 
-        if (user.password === password) {
-          const token = jwt.sign({ email: user.email, userId: user.id }, secretKey, { expiresIn: '10h' });
-          activeUsers[token] = {
-            expiresIn: '10h',
-            isActive: true,
-          };
-          res.json({ user: user, token: token });
-        } else {
-          res.json({ user: "Wrong password" });
+  try {
+    const [result] = await connection.query("SELECT * FROM usersSupport");
+    res.json(result);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Error fetching user support requests");
+  }
+};
+
+const getStoriesInfo = async (req, res) => {
+  const { id } = req.body;
+  const filePath = path.join(storiesFilePath, `${id}.txt`);
+
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    res.json(JSON.parse(data));
+  } catch (err) {
+    console.error('Error reading file:', err);
+    res.status(500).json({ data: 'Error reading data.' });
+  }
+};
+
+const getMessages = async (req, res) => {
+  const { id } = req.body;
+  const filePath = path.join(messagesDir, `${id}.txt`);
+
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    res.json(data);
+  } catch (err) {
+    console.error('Error reading file:', err);
+    res.status(500).json({ error: 'Error reading message from file' });
+  }
+};
+
+const getSearchData = async (req, res) => {
+  const { value } = req.body;
+  const searchDir = path.join('backend', 'search', 'result.txt');
+
+  try {
+    const data = await fs.promises.readFile(searchDir, 'utf8');
+    const parsedData = JSON.parse(data);
+    let foundText = '';
+    let title = '';
+
+    for (let key in parsedData) {
+      if (key.toLowerCase().includes(value.toLowerCase())) {
+        foundText = parsedData[key];
+        title = key;
+        break;
+      }
+    }
+
+    if (!foundText) {
+      for (let key in parsedData) {
+        if (parsedData[key].content.toLowerCase().includes(value.toLowerCase())) {
+          foundText = parsedData[key].content;
+          title = key;
+          break;
         }
-      } else {
-        console.log('Пользователь не найден');
-        res.json({ user: null });
       }
     }
-  });
-});
 
+    res.status(200).json(foundText ? { foundText, title } : { title: null });
+  } catch (error) {
+    console.error('Error processing data:', error);
+    res.status(500).json({ error: 'Error processing data' });
+  }
+};
 
-app.post('/register', (req, res) => {
+const saveMessage = async (req, res) => {
+  const { message, date, time, name, second_name, id, status } = req.body;
+
+  if (!message.trim()) {
+    return res.status(400).json({ error: 'Message cannot be empty' });
+  }
+
+  const dataToWrite =
+    `"${time}": {
+  "name": "${name}", 
+  "second_name": "${second_name}", 
+  "date": "${date}", 
+  "message": "${message}",
+  "status": "${status}"
+},\n`;
+
+  const filePath = path.join(messagesDir, `${id}.txt`);
+
+  try {
+    await fs.promises.appendFile(filePath, dataToWrite, 'utf8');
+    res.json({ message: 'Message saved successfully' });
+  } catch (err) {
+    console.error('Error writing to file:', err);
+    res.status(500).json({ error: 'Error writing message to file' });
+  }
+};
+
+const signIn = async (req, res) => {
   const { email, password } = req.body;
 
-  connection.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
-    if (err) {
-      console.log('Error in trying to connect to database: ', err);
-      res.status(500).json({ error: 'Error in fetching' });
-    } else {
-      if (results.length > 0) {
-        res.json({ user: true });
+  try {
+    const [results] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+    if (results.length > 0) {
+      const user = results[0];
+      if (user.password === password) {
+        const token = jwt.sign({ email: user.email, userId: user.id }, secretKey, { expiresIn: '10h' });
+        activeUsers[token] = { expiresIn: '10h', isActive: true };
+        res.json({ user, token });
       } else {
-        connection.query('INSERT INTO `users`(`email`, `password`) VALUES (?, ?)', [email, password], (error, result) => {
-          if (error) {
-            console.error('Error in inserting user:', error);
-            res.status(500).json({ error: 'Error in inserting user' });
-          } else {
-            console.log('User successfully created');
-            const token = jwt.sign({ email: email, userId: result.insertId }, secretKey, { expiresIn: '10h' });
-            activeUsers[token] = {
-              expiresIn: '10h',
-              isActive: true,
-            };
-            res.json({ user: { email: email, id: result.insertId }, token: token });
-          }
-        });
+        res.json({ user: "Wrong password" });
       }
+    } else {
+      res.json({ user: null });
     }
-  });
-});
+  } catch (error) {
+    console.error('Error in fetching user:', error);
+    res.status(500).json({ error: 'Error in fetching' });
+  }
+};
 
-app.post("/saveData", (req, res) => {
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [results] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+    if (results.length > 0) {
+      res.json({ user: true });
+    } else {
+      const [result] = await connection.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password]);
+      const token = jwt.sign({ email, userId: result.insertId }, secretKey, { expiresIn: '10h' });
+      activeUsers[token] = { expiresIn: '10h', isActive: true };
+      res.json({ user: { email, id: result.insertId }, token });
+    }
+  } catch (error) {
+    console.error('Error in registering user:', error);
+    res.status(500).json({ error: 'Error in inserting user' });
+  }
+};
+
+const saveData = async (req, res) => {
   const { body } = req;
   const { email } = body;
 
-  console.log('Request body:', body);
-
   if (!email) {
-    console.log('Nickname is required');
-    return res.status(400).json({ error: 'Nickname is required' });
+    return res.status(400).json({ error: 'Email is required' });
   }
 
   const fields = Object.keys(body).filter(key => key !== 'email');
-  const values = fields.map(field => body[field]);
-
   if (fields.length === 0) {
-    console.log('No fields to update');
     return res.status(400).json({ error: 'No fields to update' });
   }
 
+  const values = fields.map(field => body[field]);
   const setClause = fields.map(field => `${field} = ?`).join(', ');
 
   const query = `UPDATE users SET ${setClause} WHERE email = ?`;
   values.push(email);
 
-  console.log('Executing query:', query, 'with values:', values);
-
-  connection.query(query, values, (err, results) => {
-    if (err) {
-      console.log('Error in trying to connect to database: ', err);
-      return res.status(500).json({ error: 'Error in updating' });
-    }
-
+  try {
+    const [results] = await connection.query(query, values);
     if (results.affectedRows === 0) {
-      console.log('User not found');
-      return res.status(404).json({ error: 'User not found' });
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.status(200).json({ message: 'Data updated successfully' });
     }
+  } catch (error) {
+    console.error('Error in updating user:', error);
+    res.status(500).json({ error: 'Error in updating' });
+  }
+};
 
-    console.log('Data updated successfully');
-    res.status(200).json({ message: 'Data updated successfully' });
-  });
-});
+const getUsersData = async (req, res) => {
+  const query = "SELECT name, second_name, status FROM users WHERE status IN ('user')";
 
-app.post("/getUsersData", (req, res) => {
-  const query = `
-    SELECT name, second_name, status 
-    FROM users 
-    WHERE status IN ('user')
-  `;
+  try {
+    const [results] = await connection.query(query);
+    res.json(results.map(row => ({ name: row.name, second_name: row.second_name, status: row.status })));
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-  connection.query(query, (error, results) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
+const getCustomersData = async (req, res) => {
+  const query = "SELECT name, second_name, status FROM users WHERE status IN ('customer', 'tech', 'admin')";
 
-    const response = results.map(row => ({
-      name: row.name,
-      second_name: row.second_name,
-      status: row.status
-    }));
+  try {
+    const [results] = await connection.query(query);
+    res.json(results.map(row => ({ name: row.name, second_name: row.second_name, status: row.status })));
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    res.json(response);
-  });
-})
-
-app.post("/getUserAppointmentsData", (req, res) => {
-
-})
-
-app.post("/getCustomersData", (req, res) => {
-  const query = `
-    SELECT name, second_name, status 
-    FROM users 
-    WHERE status IN ('customer', 'tech', 'admin')
-  `;
-
-  connection.query(query, (error, results) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-
-    const response = results.map(row => ({
-      name: row.name,
-      second_name: row.second_name,
-      status: row.status
-    }));
-
-    res.json(response);
-  });
-});
-
-app.post('/logout', (req, res) => {
-  let { token } = req.body;
+const logout = (req, res) => {
+  const { token } = req.body;
   delete activeUsers[token];
-  console.log(activeUsers);
   res.json({ message: 'Logged out successfully' });
-});
+};
+
+// Routes
+app.post('/userSupportRequests', userSupportRequests);
+app.post('/getStoriesInfo', getStoriesInfo);
+app.post('/getMessages', getMessages);
+app.post('/getSearchData', getSearchData);
+app.post('/saveMessage', saveMessage);
+app.post('/signIn', signIn);
+app.post('/register', register);
+app.post('/saveData', saveData);
+app.post('/getUsersData', getUsersData);
+app.post('/getCustomersData', getCustomersData);
+app.post('/logout', logout);
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
